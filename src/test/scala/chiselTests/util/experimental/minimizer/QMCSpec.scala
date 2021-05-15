0bf35a1227fbe5dc5769e9e6a6a3992ddacfd060
Jiuyang Liu <liu@jiuyang.me>
2021-05-15 15:46:08 +0000

    implement test for qmc

---
+++ src/test/scala/chiselTests/util/experimental/minimizer/QMCSpec.scala
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: Apache-2.0
+
+package chiselTests.util.experimental.minimizer
+import chisel3.util.experimental.decode.Minimizer
+import chisel3.util.experimental.decode.QMCMinimizer
+
+class QMCSpec extends MinimizerSpec {
+  override def minimizer: Minimizer = QMCMinimizer
+
+  "case" should "pass" in {
+    minimizerTest(multiDefaultCase, "multiDefaultCase")
+  }
+}
